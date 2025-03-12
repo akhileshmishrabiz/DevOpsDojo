7c86bc1ba9ced5605fadcd91d5cf03d2dd66ac06
Akhilesh Mishra <83780705+akhileshmishrabiz@users.noreply.github.com>
2025-03-12 21:09:28 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@db:5432/devops_learning')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@db:5432/devops_learning')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgresadmin:YourStrongPassword123!@postgres-db.3-tier-app-eks.svc.cluster.local:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = bool(int(os.getenv('FLASK_DEBUG', '0')))
