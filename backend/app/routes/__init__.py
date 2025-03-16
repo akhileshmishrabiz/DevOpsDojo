547eb2d14c8efc7a4a0e8d15174822158ccaae72
Akhilesh Mishra <83780705+akhileshmishrabiz@users.noreply.github.com>
2025-03-16 16:14:53 +0530

    Update __init__.py

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -4,7 +4,7 @@ from flask import Blueprint, jsonify
 topic_bp = Blueprint('topics', __name__, url_prefix='/api/topics')
 quiz_bp = Blueprint('quizzes', __name__, url_prefix='/api/quiz')
 
-api_bp = Blueprint('api', __name__, url_prefix='/')
+api_bp = Blueprint('api', __name__, url_prefix='/api')
 
 # Add a simple health check route
 @api_bp.route('', methods=['GET'])
